7d5cea70d0ea62d9a33c5fe92393720fcb7a7ef7
Egor <egorcheg1@gmail.com>
2021-04-30 22:59:45 +0300

    WIP(app): 🚧 добавляем action auth/login

--- src/features/Auth/actions.ts
+++ src/features/Auth/actions.ts
@@ -2,15 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // types
 import { Maybe, ThunkAPI } from "models/types";
+import { User as IUser } from "@accounts/types";
 
-interface ILogin {
+interface ISingup {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
 }
 
-export const signup = createAsyncThunk<Maybe<string>, ILogin, ThunkAPI>(
+export const signup = createAsyncThunk<Maybe<string>, ISingup, ThunkAPI>(
   "auth/signup",
   async (
     { firstName, lastName, email, password },
@@ -34,4 +35,31 @@ export const signup = createAsyncThunk<Maybe<string>, ILogin, ThunkAPI>(
   }
 );
 
-// TODO: добавить login, logout
+interface ILogin {
+  email: string;
+  password: string;
+}
+
+export const login = createAsyncThunk<IUser, ILogin, ThunkAPI>(
+  "auth/login",
+  async (
+    { email, password },
+    { extra: { accountsPassword }, rejectWithValue }
+  ) => {
+    try {
+      const { user } = await accountsPassword.login({
+        user: {
+          email,
+        },
+        password,
+      });
+
+      return user;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+// TODO: добавить logout
