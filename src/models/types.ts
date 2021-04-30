67ef17be6187bf2428d985b6676945408f7cb811
Egor <egorcheg1@gmail.com>
2021-04-30 22:57:58 +0300

    WIP(app): 🚧 исправляем опечатку accaunts на accounts

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,6 +1,6 @@
 // types
 import { IAppDispatch } from "app/store";
-import { IAccountsPassword } from "lib/accaunts";
+import { IAccountsPassword } from "lib/accounts";
 
 export type Maybe<T> = T | null;
 export type ThunkAPI = {
