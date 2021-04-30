1bbdcbd9e46c7ff27fae88caeb340ccd7aa46869
Egor <egorcheg1@gmail.com>
2021-04-30 22:38:16 +0300

    WIP(common): 🚧 добавляем PrivateRoute
    PrivateRoute проверяем если пользователь не залогинен, то редиректит на
    /login

---
+++ src/common/PrivateRoute.tsx
@@ -0,0 +1,15 @@
+import { Redirect, Route, RouteProps } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+// types
+import { RootState } from "app/store";
+
+export const PrivateRoute: React.FC<RouteProps> = (props) => {
+  const loggedUser = useSelector((state: RootState) => state.auth.loggedUser);
+
+  if (!loggedUser) {
+    return <Redirect to="/login" />;
+  }
+
+  return <Route {...props} />;
+};
