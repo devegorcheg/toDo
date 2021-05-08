1bbbc5a4df6d057ab33761fbedfdeff34aa82a5c
Egor <egorcheg1@gmail.com>
2021-05-09 00:06:06 +0300

    WIP(app): 🚧 поправляем отступы на форме авторизации и входа. Исправляем импорт PrivateRoute

--- src/features/App/components/Routes.tsx
+++ src/features/App/components/Routes.tsx
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 // components
 import { Signup } from "features/Auth/components/Signup";
 import { Login } from "features/Auth/components/Login";
-import { PrivateRoute } from "common/PrivateRoute";
+import { PrivateRoute } from "common/components/PrivateRoute";
 import { RootPage } from "./RootPage";
 
 export const Routes: React.FC = () => {
