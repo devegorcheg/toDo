6921c25a01678256feed243b5dd5ba63dc66faa1
Egor <egorcheg1@gmail.com>
2021-10-27 19:19:39 +0300

    WIP(user): 🚧 Выносим форму в отдельный компонент

---
+++ src/features/Auth/components/Wrapper.tsx
@@ -0,0 +1,40 @@
+// component
+import { Box, BoxProps, Container, Paper } from "@mui/material";
+
+// style
+import { styled } from "@mui/system";
+import { deepPurple } from "@mui/material/colors";
+
+const StyledBox = styled(Box)(() => ({
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  flexShrink: 0,
+  background: deepPurple[200],
+  "&:before": {
+    content: "''",
+    display: "block",
+    flexGrow: 1,
+    minHeight: "30px",
+  },
+  "&:after": {
+    content: "''",
+    display: "block",
+    flexGrow: 1,
+    minHeight: "30px",
+  },
+}));
+
+export const Wrapper: React.FC<BoxProps> = ({ children, ...props }) => {
+  return (
+    <StyledBox {...props}>
+      <Container maxWidth="sm">
+        <Paper elevation={5}>
+          <Box px={5} py={5}>
+            {children}
+          </Box>
+        </Paper>
+      </Container>
+    </StyledBox>
+  );
+};
